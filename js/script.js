c548dd71b360ecdc5202504518487ef33ed5ee87
Riddhima Singhal <singhalriddhima08@gmail.com>
2025-08-14 11:00:12 +0530

    div changes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-
+/* ==================== typing animation ================================= */
+var typed = new Typed(".typing",{
+    strings:["Software Developer","Web Designer","Web Developer", "Graphic Designer"],
+    typespeed:100,
+    Backspeed:60,
+    loop:true
+})
